2ecd2abcfceee621e49ea631f4c1814d05250fd5
Marcel Breuer <breuer.marcel@outlook.com>
2024-01-23 20:02:10 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    optimizeDeps: {
+        disabled: false,
+    },
+    build: {
+        commonjsOptions: { include: [] },
+    },
+});
